8a57aa379ce60cc7a51ddb47cc436109f5ea7897
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-06-04 11:31:10 +0200

    edit NotFound view layout

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -8,9 +8,27 @@ export default {
 </script>
 
 <template>
-    <h1>Not found</h1>
+    <div class="container">
+        <div class="row">
+            <h1>404 | NOT FOUND</h1>
+            <router-link class="btn-lg" to="/"> Return to Home</router-link>
+        </div>
+    </div>
 </template>
 
 
 
-<style></style>
+<style scoped>
+.row {
+    height: 100vh;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    &>a {
+        text-decoration: none;
+        color: inherit;
+    }
+}
+</style>
